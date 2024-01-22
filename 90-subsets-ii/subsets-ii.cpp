b979e8cf99d71d4f26994a3854f471d2741bd7ad
Abhiramgopi1 <96803986+Abhiramgopi1@users.noreply.github.com>
2024-01-22 18:31:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (32.22%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         int n=nums.size();
-        
+        vector<int>v;
         vector<vector<int>>ans;
         int p=pow(2,n);
         for(int i=0;i<p;i++){
-            vector<int>v;
             for(int j=0;j<n;j++)
             {
                 if(i& (1<<j)){
@@ -15,6 +14,7 @@ public:
             }
             sort(v.begin(),v.end());
             ans.push_back(v);
+            v.clear();
         }
         sort(ans.begin(),ans.end());
         ans.erase(unique(ans.begin(),ans.end()),ans.end());
