046e58129932e73ef377d4a341b196c0b486e72a
Abhiramgopi1 <96803986+Abhiramgopi1@users.noreply.github.com>
2024-02-02 22:28:28 +0530

    Time: 24 ms (92.05%) | Memory: 11.5 MB (91.33%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,33 +1,34 @@
 class Solution {
-long long dp[1000001];
 public:
-    long long fun(int amount,vector<int>&a,int n){
-        if(amount==0) return 0;
-        if(dp[amount]!=-1) return dp[amount];
-        long long ans=INT_MAX;
-        for(int i=0;i<n;i++){
-            if(a[i]<=amount){
-                ans=min(ans,1+fun(amount-a[i],a,n));
+    // long long fun(int amount,vector<int>&a,int n){
+    //     if(amount==0) return 0;
+    //     if(dp[amount]!=-1) return dp[amount];
+    //     long long ans=INT_MAX;
+    //     for(int i=0;i<n;i++){
+    //         if(a[i]<=amount){
+    //             ans=min(ans,1+fun(amount-a[i],a,n));
+    //         }
+    //     }
+    //     return dp[amount]=ans;
+    // }
+    int coinChange(vector<int>& coins, int amount) {
+        sort(coins.begin(),coins.end());
+        long long dp[amount+1];
+        int n=coins.size();
+        memset(dp,1e9+1,sizeof(dp));
+        dp[0]=0;
+        for(int i=1;i<=amount;i++){
+            for(int j=0;j<n;j++){
+                if(i-coins[j]>=0){
+                    dp[i]=min(dp[i-coins[j]]+1,dp[i]);
+                }
             }
         }
-        return dp[amount]=ans;
-    }
-    int coinChange(vector<int>& coins, int amount) {
-        int n=coins.size();
-        // memset(dp,INT_MAX,sizeof(dp));
-        // dp[0]=0;
-        // for(int i=1;i<=amount;i++){
-        //     for(int j=0;j<n;j++){
-        //         if(i-coins[j]>=0){
-        //             dp[i]=min(dp[i-coins[j]]+1,dp[i]);
-        //         }
-        //     }
-        // }
-        // if(dp[amount]==INT_MAX) return -1;
-        // else return dp[amount];
-        memset(dp,-1,sizeof(dp));
-        long long m=fun(amount,coins,n);
-        if(m==INT_MAX) return -1;
-        else return m;
+        if(dp[amount]>amount) return -1;
+        else return dp[amount];
+        // memset(dp,-1,sizeof(dp));
+        // long long m=fun(amount,coins,n);
+        // if(m==INT_MAX) return -1;
+        // else return m;
     }
 };
