3859f3a314267405ddf5259513987ccdf03abb46
Abhiramgopi1 <96803986+Abhiramgopi1@users.noreply.github.com>
2024-03-05 15:29:56 +0530

    Time: 7 ms (99.66%) | Memory: 14 MB (13.45%) - LeetSync

--- 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.cpp
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -5,26 +5,19 @@ public:
         int i=0 , n = s.length();
         int j=n-1;
         int c=0;
-        while(i<=j){
+        while(i<j){
             
             if(s[i]==s[j]){
-                if(i+1 == j) return 0;
-                if(i<n-1 and s[i]==s[i+1]){
-                    i++;
-                }
-                else if(j>0 and s[j]==s[j-1]){
-                    j--;
-                }
-                else {
-                    i++;
-                    j--;
-                }
+                while(i<n-1 and s[i]==s[i+1]) i++;
+                while(j>0 and s[j]==s[j-1]) j--;
+                i++;
+                j--;
             }
             else{
                 return j-i+1;
             }
         }
-        if(i>j) return 1;
+        if(i==j) return 1;
         return 0;
         
     }
