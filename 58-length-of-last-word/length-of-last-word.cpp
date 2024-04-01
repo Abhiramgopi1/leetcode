a446b43bade0d2db8c89443091ac119dea18df16
Abhiramgopi1 <96803986+Abhiramgopi1@users.noreply.github.com>
2024-04-01 21:35:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (6.02%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -5,7 +5,7 @@ public:
         int n=s.length();
         string s1="";
         for(int i=0;i<n;i++){
-            if((s[i]>='a' and s[i]<='z') or (s[i]>='A' and s[i]<='Z') ){
+            if(s[i]!=' '){
                 s1+=s[i];
             }
             else{
@@ -14,9 +14,6 @@ public:
             }
         } 
         if(s1!="")  v.push_back(s1);
-        // for(auto i: v){
-        //     cout<<i<<endl;
-        // }
         int k=v.size();
         int l=v[k-1].length();
         return l;
