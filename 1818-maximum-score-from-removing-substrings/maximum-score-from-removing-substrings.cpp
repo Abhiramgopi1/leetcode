a0c112d33300f461f54951559dee8999e6b39394
Abhiramgopi1 <96803986+Abhiramgopi1@users.noreply.github.com>
2024-07-12 13:48:19 +0530

    Time: 97 ms (46.75%) | Memory: 22.8 MB (50.81%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -3,101 +3,49 @@ public:
     int maximumGain(string s, int x, int y) {
         stack<char>st;
         long long ans=0;
-        string t="";
-        string se="";
-        string t1="";
         if(x>=y){
-            for(auto i : s){
-                st.push(i);
-                t+=st.top();
-                if(t.size()==2 and t=="ab"){
+            for(auto i: s){
+                if(!st.empty() and i=='b' and st.top()=='a'){
                     ans+=x;
                     st.pop();
-                    st.pop();
-                    if(!st.empty()){
-                        string l="";
-                        l+=st.top();
-                        t=l;
-                    }
                 }
-                else if(t.size()>=2 and t!="ab"){
-                    string e="";
-                    e+=t[1];
-                    t=e;
-                }
+                else st.push(i);
             }
+            s="";
             while(!st.empty()){
-                se+=st.top();
+                s+=st.top();
                 st.pop();
             }
-            reverse(se.begin(),se.end());
-            for(auto i : se){
-                st.push(i);
-                t1+=st.top();
-                if(t1.size()==2 and t1=="ba"){
+            for(auto i: s){
+                if(!st.empty() and i=='b' and st.top()=='a'){
                     ans+=y;
                     st.pop();
-                    st.pop();
-                    if(!st.empty()){
-                        string l="";
-                        l+=st.top();
-                        t1=l;
-                    }
                 }
-                else if(t1.size()>=2 and t1!="ba"){
-                    string e="";
-                    e+=t1[1];
-                    t1=e;
-                }
+                else st.push(i);
             }
         }
-        else{
-             for(auto i : s){
-                st.push(i);
-                t+=st.top();
-                if(t.size()==2 and t=="ba"){
+        else {
+            for(auto i: s){
+                if(!st.empty() and i=='a' and st.top()=='b'){
                     ans+=y;
                     st.pop();
-                    st.pop();
-                    if(!st.empty()){
-                        string l="";
-                        l+=st.top();
-                        t=l;
-                    }
                 }
-                else if(t.size()>=2 and t!="ba"){
-                    string e="";
-                    e+=t[1];
-                    t=e;
-                }
+                else st.push(i);
             }
+            s="";
             while(!st.empty()){
-                se+=st.top();
+                s+=st.top();
                 st.pop();
             }
-            reverse(se.begin(),se.end());
-            for(auto i : se){
-                st.push(i);
-                t1+=st.top();
-                if(t1.size()==2 and t1=="ab"){
+            for(auto i: s){
+                if(!st.empty() and i=='a' and st.top()=='b'){
                     ans+=x;
                     st.pop();
-                    st.pop();
-                    if(!st.empty()){
-                        string l="";
-                        l+=st.top();
-                        t1=l;
-                    }
                 }
-                else if(t1.size()>=2 and t1!="ab"){
-                    string e="";
-                    e+=t1[1];
-                    t1=e;
-                }
+                else st.push(i);
             }
-
         }
-        if(x==2347 || x==6129) return ans+x;
         return ans;
+        
     }
 };
