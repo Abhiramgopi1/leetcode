f6ee5806321fb7431ef8d7647569602719650fc5
Abhiramgopi1 <96803986+Abhiramgopi1@users.noreply.github.com>
2024-07-20 10:48:42 +0530

    Time: 45 ms (15.80%) | Memory: 36.1 MB (11.51%) - LeetSync

--- 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.cpp
+++ 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.cpp
@@ -4,10 +4,6 @@ public:
         int n=rowSum.size();
         int m=colSum.size();
         vector<vector<int>>ans(n,vector<int>(m,0));
-        int r=INT_MAX;
-        for(auto i: rowSum){
-            r=min(r,i);
-        }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(rowSum[i]<colSum[j]){
