1462fe6e9576a3014f7f522f232c0b404510e817
Abhiramgopi1 <96803986+Abhiramgopi1@users.noreply.github.com>
2024-07-24 14:27:10 +0530

    Time: 147 ms (89.18%) | Memory: 88.9 MB (49.78%) - LeetSync

--- 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.cpp
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
-        map<int,vector<int>>mp;
-        for(auto i: nums){
-            string s=to_string(i);
+    vector<int> sortJumbled(vector<int>&mapping, vector<int>& nums) {
+        vector<pair<int,int>>mp;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            string s=to_string(nums[i]);
             int l=0;
             for(auto i: s){
                 int a=i-'0';
                 l=l*10+mapping[a];
             }
-            mp[l].push_back(i);
-
+            mp.push_back({l,i});
         }
-        vector<int>a;
-        
 
+        sort(mp.begin(),mp.end());
+        vector<int>a;
         for(auto i: mp){
-            for(auto j: i.second){
-                a.push_back(j);
-            }
+            int k=i.second;
+            a.push_back(nums[k]);
         }
         return a;
     }
