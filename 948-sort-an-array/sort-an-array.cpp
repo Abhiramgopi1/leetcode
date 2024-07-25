8112317024c3f292ca8688223871bf46e45f0647
Abhiramgopi1 <96803986+Abhiramgopi1@users.noreply.github.com>
2024-07-25 14:28:40 +0530

    Time: 161 ms (55.34%) | Memory: 78.6 MB (49.24%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,8 +1,24 @@
 class Solution {
 public:
-    
+    void rev(vector<int>&nums){
+        int i=0,j=nums.size()-1;
+        while(i<=j){
+            swap(nums[i],nums[j]);
+            i++;
+            j--;
+        } 
+    }
     vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        return nums;
+        priority_queue<int>q;
+        for(auto i:nums){
+            q.push(i);
+        }
+        vector<int>ans;
+        while(!q.empty()){
+            ans.push_back(q.top());
+            q.pop();
+        }
+        rev(ans);
+        return ans;
     }
 };
