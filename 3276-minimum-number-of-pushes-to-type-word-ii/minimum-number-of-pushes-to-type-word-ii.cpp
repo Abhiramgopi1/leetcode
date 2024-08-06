ad3dae15f73d331fdda106414f3ab0b74a8ba8d4
Abhiramgopi1 <96803986+Abhiramgopi1@users.noreply.github.com>
2024-08-06 10:30:27 +0530

    Time: 124 ms (23.42%) | Memory: 29.4 MB (5.55%) - LeetSync

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        unordered_map<char,int>mp;
+        for(auto i: word){
+            mp[i]++;
+        }
+
+        vector<int>v;
+
+        for(auto i: mp){
+            v.push_back(i.second);
+        } 
+
+        sort(v.rbegin(),v.rend());
+
+        int j=0,p=1,c=0;
+        for(auto i: v){
+            cout<<i<<" ";
+            c+=p*i;
+            j++;
+            if(j==8) p=2;
+            if(j==16) p=3;
+            if(j==24) p=4;
+        }
+
+        return c;
+    }
+};
