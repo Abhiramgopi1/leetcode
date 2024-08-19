7544b1ae9eee748213bb93c6369823ce065bdc0c
Abhiramgopi1 <96803986+Abhiramgopi1@users.noreply.github.com>
2024-08-19 15:00:07 +0530

    Time: 4 ms (55.28%) | Memory: 7.9 MB (45.58%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -2,14 +2,12 @@ class Solution {
 public:
     int minSteps(int n) {
         string s="A",ps=s;
-        int c=0,i=1;
+        int c=0;
         while(s.length()<n){
             if(n%s.length()==0){
-                i=s.length();
                 ps=s;
                 c+=1;
             }
-            int k=i;
             s+=ps;
             c+=1;
         }
