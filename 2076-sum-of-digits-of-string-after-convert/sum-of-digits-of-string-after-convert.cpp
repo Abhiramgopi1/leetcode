db6f29004f2a0392e2ada842074f4ec177cc994e
Abhiramgopi1 <96803986+Abhiramgopi1@users.noreply.github.com>
2024-09-03 22:48:34 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (5.53%) - LeetSync

---
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int getLucky(string s, int k) {
+        string s1="";
+        unordered_map<int,int>mp;
+        int c=1;
+        for(int i=49;i<=75;i++){
+            mp[i]=c;
+            c++;
+        }
+        for(auto i: s){
+            int k=i-'0';
+            s1+=to_string(mp[k]);
+        }
+
+        long long ans=0;
+        
+        while(k--){
+            ans=0;
+            for(auto i: s1){
+                string g=to_string(i);
+                ans+=mp[stoi(g)];
+            }
+            s1=to_string(ans);
+        }
+
+
+        
+
+        return ans;
+    }
+};
